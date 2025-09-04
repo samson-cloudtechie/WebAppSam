1b3d8d7be38f40baafcc08c58ddb6106aa8c60bf
samson-cloudtechie <samson.cloudtechie@gmail.com>
2025-09-04 18:53:11 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SamWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+"# WebAppSam" 
+"# WebAppSam" 
