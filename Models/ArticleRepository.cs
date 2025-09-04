604263702575758896b01f2ae66105e62a18b281
samson-cloudtechie <samson.cloudtechie@gmail.com>
2025-09-04 20:23:24 +0530

    CommitMessage

---
+++ Models/ArticleRepository.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebApp.Models;
+
+namespace WebApp
+{
+    public class ArticleRepository
+    {
+        private List<Article> articles = new List<Article>
+            {
+                new Article
+                {
+                    Id = 1,
+                    Title = "What is Lorem Ipsum?",
+                    Author= "Gaurav Gahlot",
+                    PublishedOn = new DateTime(2019, 01, 20),
+                    Content = "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                },
+            };
+
+
+            public List<Article> GetLatest()
+        {
+            return articles;
+        }
+    }
+}
+
+
+
